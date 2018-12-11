b59ad98833e1208200a55c102b7b309174145c87
eshwarne <northeasteditsofficial@gmail.com>
2018-12-11 15:48:08 +0530

    Partition function implemented

--- Quick Sort/src/sorts/QuickSort.java
+++ Quick Sort/src/sorts/QuickSort.java
@@ -21,6 +21,12 @@ public class QuickSort {
 		
 	}
 	public static void main(String cliArgs[]) {
-		int[] Array= {2,5,4,2,8,7,10,5};
+		int[] Array= {2,11,4,2,8,7,10,5};
+		int pivotIndex=partition(Array,0,Array.length-1);
+		for(int ele:Array) {
+			System.out.print(ele+" ");
+		}
+		System.out.println();
+		System.out.print("Pivot Index = "+pivotIndex);
 	}
 }
