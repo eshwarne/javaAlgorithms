36147b517e33667096229eceb338b97ef18bea8a
eshwarne <northeasteditsofficial@gmail.com>
2018-12-11 15:56:56 +0530

    Quick Sort

--- Quick Sort/src/sorts/QuickSort.java
+++ Quick Sort/src/sorts/QuickSort.java
@@ -18,15 +18,23 @@ public class QuickSort {
 		return pivotIndex;
 	}
 	public static void quickSort(int[] Array, int start, int end) {
-		
+		if(start>=end) {
+			return;
+		}
+		int pivotIndex=partition(Array,start,end);
+		quickSort(Array,start,pivotIndex-1);
+		quickSort(Array,pivotIndex+1,Array.length-1);
 	}
 	public static void main(String cliArgs[]) {
 		int[] Array= {2,11,4,2,8,7,10,5};
-		int pivotIndex=partition(Array,0,Array.length-1);
+//		int pivotIndex=partition(Array,0,Array.length-1);
+//		for(int ele:Array) {
+//			System.out.print(ele+" ");
+//		}
+		quickSort(Array,0,Array.length-1);
+		System.out.println();
 		for(int ele:Array) {
 			System.out.print(ele+" ");
 		}
-		System.out.println();
-		System.out.print("Pivot Index = "+pivotIndex);
 	}
 }
