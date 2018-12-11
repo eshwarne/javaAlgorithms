945c1bd87c21c1d04c6eaef6c6ae17b199498a1a
eshwarne <northeasteditsofficial@gmail.com>
2018-12-11 15:44:27 +0530

    Quick sort Paritition function

---
+++ Quick Sort/src/sorts/QuickSort.java
@@ -0,0 +1,26 @@
+package sorts;
+
+public class QuickSort {
+	public static int partition(int[] Array,int start,int end) {
+		int pivotIndex = start;
+		int pivotElement=Array[end];
+		for(int i=start;i<=(end-1);i++) {
+			if(Array[i]<pivotElement) {
+				int temp=Array[pivotIndex];
+				Array[pivotIndex]=Array[i];
+				Array[i]=temp;
+				pivotIndex++;
+			}
+		}
+		int temp=Array[pivotIndex];
+		Array[pivotIndex]=Array[end];
+		Array[end]=temp;
+		return pivotIndex;
+	}
+	public static void quickSort(int[] Array, int start, int end) {
+		
+	}
+	public static void main(String cliArgs[]) {
+		
+	}
+}
